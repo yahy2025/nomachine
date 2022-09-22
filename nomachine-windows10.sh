f9db81bcd322e528ecde777f8e82c9936db30b15
yahy2025 <113997287+yahy2025@users.noreply.github.com>
2022-09-22 02:56:28 +0200

    Create nomachine-windows10.sh

---
+++ nomachine-windows10.sh
@@ -0,0 +1,36 @@
+wget -O ng.sh https://github.com/yahy2025/nomachine/raw/main/ngrok.sh > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+
+
+function goto
+{
+    label=$1
+    cd 
+    cmd=$(sed -n "/^:[[:blank:]][[:blank:]]*${label}/{:a;n;p;ba};" $0 | 
+          grep -v ':$')
+    eval "$cmd"
+    exit
+}
+
+: ngrok
+clear
+
+CRP=2F2AAFU0RUFjAnwsmRqmolHIj4Q_6qFuZehK1VKzGQ2wpt4hM
+./ngrok authtoken $CRP 
+
+clear
+CRP=eu
+./ngrok tcp --region $CRP 4000 &>/dev/null &
+sleep 1
+if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
+docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10
+clear
+echo "NoMachine: https://www.nomachine.com/download"
+echo Done! NoMachine Information:
+echo IP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
+echo User: user
+echo Passwd: 123456
+echo "VM can't connect? Restart Cloud Shell then Re-run script."
+seq 1 86400 | while read i; do echo -en "\r Running .     $i s /86400 s";sleep 0.1;echo -en "\r Running ..    $i s /86400 s";sleep 0.1;echo -en "\r Running ...   $i s /86400 s";sleep 0.1;echo -en "\r Running ....  $i s /86400 s";sleep 0.1;echo -en "\r Running ..... $i s /86400 s";sleep 0.1;echo -en "\r Running     . $i s /86400 s";sleep 0.1;echo -en "\r Running  .... $i s /86400 s";sleep 0.1;echo -en "\r Running   ... $i s /86400 s";sleep 0.1;echo -en "\r Running    .. $i s /86400 s";sleep 0.1;echo -en "\r Running     . $i s /86400 s";sleep 0.1; done
