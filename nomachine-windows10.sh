dc9469d10d431367eb39c7196db6508f14a934a4
yahy2025 <113997287+yahy2025@users.noreply.github.com>
2022-09-22 03:01:57 +0200

    Update nomachine-windows10.sh

--- nomachine-windows10.sh
+++ nomachine-windows10.sh
@@ -16,12 +16,10 @@ function goto
 : ngrok
 clear
 
-CRP=2F2AAFU0RUFjAnwsmRqmolHIj4Q_6qFuZehK1VKzGQ2wpt4hM
+CRP=2F02TjUcd0E8CjhiGl2QyJhm49C_2QjaJSF85gzAcWw4G45jF
 ./ngrok authtoken $CRP 
 
-clear
-CRP=eu
-./ngrok tcp --region $CRP 4000 &>/dev/null &
+
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
 docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g thuonghai2711/nomachine-ubuntu-desktop:windows10
